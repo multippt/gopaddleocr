d7cb1e12a295d7e891661a065f3d0f9a7497657f
Nicholas Kwan <ready725@gmail.com>
2026-03-19 22:47:09 +0800

    Fix model download for Mac

--- download_models.sh
+++ download_models.sh
@@ -3,18 +3,22 @@
 
 mkdir -p models
 
-declare -A MODELS=(
-  ["ch_PP-OCRv5_rec_server_infer.onnx"]="https://www.modelscope.cn/models/RapidAI/RapidOCR/resolve/master/onnx/PP-OCRv5/rec/ch_PP-OCRv5_rec_server_infer.onnx"
-  ["ch_PP-OCRv5_server_det.onnx"]="https://www.modelscope.cn/models/RapidAI/RapidOCR/resolve/master/onnx/PP-OCRv5/det/ch_PP-OCRv5_server_det.onnx"
-  ["ch_ppocr_mobile_v2.0_cls_infer.onnx"]="https://www.modelscope.cn/models/RapidAI/RapidOCR/resolve/master/onnx/PP-OCRv4/cls/ch_ppocr_mobile_v2.0_cls_infer.onnx"
-  ["PP-DocLayoutV3.onnx"]="https://huggingface.co/alex-dinh/PP-DocLayoutV3-ONNX/resolve/main/PP-DocLayoutV3.onnx"
+FILES=(
+  "ch_PP-OCRv5_rec_server_infer.onnx"
+  "ch_PP-OCRv5_server_det.onnx"
+  "ch_ppocr_mobile_v2.0_cls_infer.onnx"
+  "PP-DocLayoutV3.onnx"
 )
 
-(
-cd models
-for file in "${!MODELS[@]}"; do
-  if [ ! -f "$file" ]; then
-    curl -Lo "$file" "${MODELS[$file]}"
+URLS=(
+  "https://www.modelscope.cn/models/RapidAI/RapidOCR/resolve/master/onnx/PP-OCRv5/rec/ch_PP-OCRv5_rec_server_infer.onnx"
+  "https://www.modelscope.cn/models/RapidAI/RapidOCR/resolve/master/onnx/PP-OCRv5/det/ch_PP-OCRv5_server_det.onnx"
+  "https://www.modelscope.cn/models/RapidAI/RapidOCR/resolve/master/onnx/PP-OCRv4/cls/ch_ppocr_mobile_v2.0_cls_infer.onnx"
+  "https://huggingface.co/alex-dinh/PP-DocLayoutV3-ONNX/resolve/main/PP-DocLayoutV3.onnx"
+)
+
+for i in "${!FILES[@]}"; do
+  if [ ! -f "models/${FILES[$i]}" ]; then
+    curl -Lo "models/${FILES[$i]}" "${URLS[$i]}"
   fi
 done
-)
